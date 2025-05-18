237096e4772d8b68d14d8d232df7b3f37d78dfab
Aaron Guo <aaron@aaronguostudio.com>
2025-05-18 17:48:08 -0600

    Fix lint errors

--- components/footer/site.vue
+++ components/footer/site.vue
@@ -5,6 +5,12 @@ import { footerData } from '~/data'
 <template>
   <div>
     <p class="text-black dark:text-zinc-300 text-base py-1 font-semibold">About This Site</p>
-    <p class="py-2 dark:text-zinc-300" v-html="footerData.aboutTheSite" />
+    <p class="py-2 dark:text-zinc-300">
+      {{ footerData.aboutTheSite }}
+      I built it based on the
+      <UiSafeLink :href="footerData.aboutTheSiteLink.href" target="_blank" class="text-blue-500">
+        {{ footerData.aboutTheSiteLink.text }} </UiSafeLink
+      >.
+    </p>
   </div>
 </template>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,17 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt({
-  rules: {
-    'vue/html-self-closing': 'off',
+export default withNuxt(
+  {
+    rules: {
+      'vue/html-self-closing': 'off',
+    },
   },
-})
+  {
+    // Disable no-explicit-any rule for test files
+    files: ['**/tests/**/*.ts', '**/tests/**/*.js'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+)
